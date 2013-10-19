7113202e3bb21134ad063830ee3660b1f7818e3f
PJayChen <n26021539@mail.ncku.edu.tw>
2013-10-19 07:48:04 -0700

    fix the strncmp to obey the man description

--- string-util.c
+++ string-util.c
@@ -77,14 +77,13 @@ char *strncpy(char *dest, const char *src, size_t n)
 	return dest;
 }
 
-int
-strncmp(const char *s1, const char *s2, size_t n)
+int strncmp(const char *s1, const char *s2, size_t n)
 {
     for ( ; n > 0; s1++, s2++, --n)
-	if (*s1 != *s2)
-	    return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
-	else if (*s1 == '\0')
-	    return 0;
+	    if (*s1 != *s2)
+	        return *(unsigned char *)s1 - *(unsigned char *)s2;
+	    else if (*s1 == '\0')
+	        return 0;
     return 0;
 }
 
